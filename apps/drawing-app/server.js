5b875a09e0df63010e950d70db0b761d8e74bdce
max campbell <maxc@maxc.in>
2018-09-27 17:32:40 +0200

    made drawing app sync betwen clients

--- apps/drawing-app/server.js
+++ apps/drawing-app/server.js
@@ -2,6 +2,10 @@ let hexg = new context.HexGrid();
 context.onmessage = function (message) {
     if (message.type = "update") {
         hexg.import(message.data);
+        context.postMessage({
+            type:"update",
+            data:hexg.export()
+        })
     }
 }
 context.getState = function () {
