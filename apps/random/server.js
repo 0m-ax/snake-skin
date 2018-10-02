9d0070e68ee8cbb0a97669162d42605e3c7870f6
max campbell <maxc@maxc.in>
2018-10-02 21:45:52 +0200

    Improved random mode

--- apps/random/server.js
+++ apps/random/server.js
@@ -12,15 +12,38 @@ for (let i = 0; i < 15; i++) {
     }
 }
 let color = [random(0, 255), random(0, 255), random(0, 255)];
-setInterval(() => {
+function changeColor(){
+    color = [random(0, 255), random(0, 255), random(0, 255)];;
+}
+function changeHex(){
     let hex = hexg.get(random(0, 17), random(0, 11));
     if (hex) {
         hex.color = color;
     }
-}, basewait)
-setInterval(() => {
-    color = [random(0, 255), random(0, 255), random(0, 255)];
-}, basewait * 100);
+}
+let changeHexTime = 1000;
+let changeColorTime = 100*1000;
+let changeHexInterval = setInterval(changeHex, changeHexTime)
+let changeColorInterval = setInterval(changeColor, changeColorTime);
+context.onmessage = function (message) {
+    if (message.type == "update") {
+        changeHexTime = message.data.changeHexTime;
+        changeColorTime = message.data.changeColorTime;
+        clearInterval(changeHexInterval);
+        clearInterval(changeColorInterval);
+        changeHexInterval = setInterval(changeHex, changeHexTime)
+        changeColorInterval = setInterval(changeColor, changeColorTime);
+    } 
+    if(message.type == "sync" || message.type == "update"){
+        context.postMessage({
+            type:"update",
+            data:{
+                changeColorTime,
+                changeHexTime
+            }
+        })
+    }
+}
 context.getState = function () {
     return hexg.export()
 }
